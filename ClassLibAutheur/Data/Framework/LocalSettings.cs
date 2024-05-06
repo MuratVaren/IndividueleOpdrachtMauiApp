e4584e184ae5b5a6c7bf9918b289c6868d4631f8
Murat Varen <12201122@student.pxl.be>
2024-05-06 13:04:50 +0200

    connection string added for both

--- ClassLibAutheur/Data/Framework/LocalSettings.cs
+++ ClassLibAutheur/Data/Framework/LocalSettings.cs
@@ -8,14 +8,24 @@ namespace ClassLibAutheur.Data.Framework
 {
     static class LocalSettings
     {
+        // laptop connection
         public static string GetConnectionString()
         {
-            //string connectionString = "Trusted_Connection=True;;
             string connectionString = "user id = sa;";
             connectionString += "Password = pxl;";
-            connectionString += $@"Server=DESKTOP-SEAUBM4\SQLEXPRES;";
+            connectionString += $@"Server=DESKTOP-MURAT\SQLEXPRES;";
             connectionString += $"Database=DB_MURAT_VAREN";
             return connectionString;
         }
+        // pc connection
+
+        //public static string GetConnectionString()
+        //{
+        //    string connectionString = "user id = sa;";
+        //    connectionString += "Password = pxl;";
+        //    connectionString += $@"Server=DESKTOP-SEAUBM4\SQLEXPRES;";
+        //    connectionString += $"Database=DB_MURAT_VAREN";
+        //    return connectionString;
+        //}
     }
 }
